43b994865dce8719d132b355c80ffcf0169d14f9
saiful <saiful01636758237@gmail.com>
2021-08-30 10:32:08 +0600

    ajax model design complete

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,9 +1,13 @@
 (function($){
 	$(document).ready(function(){
 
-		
+		//single data show modal
+		$('a#singleshow').click(function(){
+			$('#single_modal_student').modal('show');
+		});
 
-		
+
+
 
 
 
